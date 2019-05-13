357e9e0e51cfc2376266075fc962c1db4cdef386
Simon Schwichtenberg <schwicht@mail.upb.de>
2019-05-13 09:23:09 +0200

    view/* => model/*

--- angular-app/src/view/DomainModelReferenceSegmentBase.ts
+++
@@ -1,7 +0,0 @@
-import {EClass} from "ecore/EClass";
-import {NotificationChain} from "ecore/NotificationChain";
-import {ENotificationImpl} from "ecore/ENotificationImpl";
-import {NotificationImpl} from "ecore/NotificationImpl";
-import {InternalEObject} from "ecore/InternalEObject";
-import {BasicEObjectImpl} from "ecore/BasicEObjectImpl";
-import {ModelPackageLiterals} from "./ModelPackageLiterals";
